bab5789fc33d2d9dea41e4b7d93bfee7dc96826a
Douglas Poma <douglaspoma@yahoo.com>
2023-12-23 11:23:15 -0300

    feat: inject db connection pool

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use rocket::get;
+use rocket_db_pools::{Connection, Database};
+
 mod models;
 mod repositories;
 mod schema;
-fn main() {
-    println!("Hello, world");
+
+#[derive(Database)]
+#[database("postgres")]
+struct DbConn(rocket_db_pools::diesel::PgPool);
+
+#[get("/rustaceans")]
+fn get_rustaceans(db: Connection<DbConn>) {}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", rocket::routes![get_rustaceans])
+        .attach(DbConn::init())
+        .launch()
+        .await;
 }
